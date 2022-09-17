2fe65aa20450a51954af78d6441b90fa0dc4bf1a
M Anang R <44345333+pycodeDev@users.noreply.github.com>
2022-09-17 12:19:50 +0700

    first init

---
+++ pages/home/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- Content -->
+        Bismillah
+        <p>
+            {{ nama }}
+        </p>
+        <button class="border text-red-500" @click.prevent="tambah()" >a</button>
+        <br/>
+
+        <Tutorial :dataTutorial="data_tutorial" />
+
+    </div>
+</template>
+
+<script>
+    export default {
+    name: "HomeIndex",
+    data() {
+        return {
+            nama: "Home Index",
+            data_tutorial: "Tes anang"
+        };
+    },
+    methods: {
+        tambah() {
+            this.nama = 1 + 2;
+        }
+    }
+}
+</script>
